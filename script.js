faec5e09a9ce03434ce57095fbc40b54ecc53b48
Rashmi <Rashmi@2006>
2026-05-13 17:16:36 +0530

    project is complete.

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-let button = document.querySelector(".btn1");
 let input = document.querySelector("#inp1");
 let ul = document.querySelector(".tasklist");
-function addnewtask() {
-  let li = document.createElement("li");
-  let inp_val = input.value;
-  li.innerText = inp_val;
-  ul.appendChild(li);
-  let img2=document.createElement("img");
-  img2.src="cross_icon.png";
-  img2.alt="Cross image";
-  ul.appendChild(img2);
-  img2.onclick= function(){
-    li.remove();
-    img2.remove();
-  }
-// const checkbox = document.createElement("input");
-// checkbox.type = "checkbox";
-// checkbox.className = "task-check";
-// li.appendChild(checkbox);
-//  input.value="";
-}
 
+function addnewtask() {
+
+    let inp_val = input.value.trim();
+
+    if(inp_val === ""){
+        alert("Please enter a task");
+        return;
+    }
+
+    // Create task container
+    let taskItem = document.createElement("div");
+    taskItem.classList.add("task-item");
+
+    // Create li
+    let li = document.createElement("li");
+    li.innerText = inp_val;
+
+    // Create delete image
+    let img2 = document.createElement("img");
+    img2.src = "cross_icon.png";
+    img2.classList.add("delete-btn");
+
+    // Append elements
+    taskItem.appendChild(li);
+    taskItem.appendChild(img2);
+
+    ul.appendChild(taskItem);
+
+    // Clear input
+    input.value = "";
+
+    // Delete task
+    img2.onclick = function () {
+        taskItem.remove();
+    };
+}
